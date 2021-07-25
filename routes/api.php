9368ecff248d2eea003ca74b055cec10efb308cb
MOISES MARTINEZ CID <20183l301006@utcv.edu.mx>
2021-07-25 02:20:50 -0500

    nueva api

--- routes/api.php
+++ routes/api.php
@@ -24,39 +24,39 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/register', [UserController::class, 'register']);
+//Route::post('/login', [UserController::class, 'login']);
+//Route::post('/register', [UserController::class, 'register']);
 Route::put('/user/update', [UserController::class, 'update']);
-Route::post('/user/upload', [UserController::class, 'upload'])->middleware(ApiAuthMiddleware::class);
+Route::post('/user/upload', [UserController::class, 'upload']);
 Route::get('/user/avatar/{filename}', [UserController::class, 'getImage']);
 Route::get('/user/detail/{id}', [UserController::class, 'detail']);
 
-Route::post('/categoria', [CategoriaController::class, 'create'])->middleware(ApiAuthMiddleware::class);
-Route::get('/categorias', [CategoriaController::class, 'list'])->middleware(ApiAuthMiddleware::class);
-Route::put('/categoria/{id}', [CategoriaController::class, 'update'])->middleware(ApiAuthMiddleware::class);
-Route::delete('/categoria/{id}', [CategoriaController::class, 'delete'])->middleware(ApiAuthMiddleware::class);
-Route::get('/categoria-find/{id}', [CategoriaController::class, 'find'])->middleware(ApiAuthMiddleware::class);
-Route::put('/categoria-search', [CategoriaController::class, 'search'])->middleware(ApiAuthMiddleware::class);
+Route::post('/categoria', [CategoriaController::class, 'create']);
+Route::get('/categorias', [CategoriaController::class, 'list']);
+Route::put('/categoria/{id}', [CategoriaController::class, 'update']);
+Route::delete('/categoria/{id}', [CategoriaController::class, 'delete']);
+Route::get('/categoria-find/{id}', [CategoriaController::class, 'find']);
+Route::put('/categoria-search', [CategoriaController::class, 'search']);
 
-Route::post('/marca', [MarcaController::class, 'create'])->middleware(ApiAuthMiddleware::class);
-Route::get('/marcas', [MarcaController::class, 'list'])->middleware(ApiAuthMiddleware::class);
-Route::put('/marca/{id}', [MarcaController::class, 'update'])->middleware(ApiAuthMiddleware::class);
-Route::delete('/marca/{id}', [MarcaController::class, 'delete'])->middleware(ApiAuthMiddleware::class);
-Route::get('/marca-find/{id}', [MarcaController::class, 'find'])->middleware(ApiAuthMiddleware::class);
-Route::put('/marca-search', [MarcaController::class, 'search'])->middleware(ApiAuthMiddleware::class);
+Route::post('/marca', [MarcaController::class, 'create']);
+Route::get('/marcas', [MarcaController::class, 'list']);
+Route::put('/marca/{id}', [MarcaController::class, 'update']);
+Route::delete('/marca/{id}', [MarcaController::class, 'delete']);
+Route::get('/marca-find/{id}', [MarcaController::class, 'find']);
+Route::put('/marca-search', [MarcaController::class, 'search']);
 
-Route::post('/distribuidor', [DistribuidorController::class, 'create'])->middleware(ApiAuthMiddleware::class);
-Route::get('/distribuidores', [DistribuidorController::class, 'list'])->middleware(ApiAuthMiddleware::class);
-Route::put('/distribuidor/{id}', [DistribuidorController::class, 'update'])->middleware(ApiAuthMiddleware::class);
-Route::delete('/distribuidor/{id}', [DistribuidorController::class, 'delete'])->middleware(ApiAuthMiddleware::class);
-Route::get('/distribuidor-find/{id}', [DistribuidorController::class, 'find'])->middleware(ApiAuthMiddleware::class);
-Route::put('/distribuidor-search', [DistribuidorController::class, 'search'])->middleware(ApiAuthMiddleware::class);
+Route::post('/distribuidor', [DistribuidorController::class, 'create']);
+Route::get('/distribuidores', [DistribuidorController::class, 'list']);
+Route::put('/distribuidor/{id}', [DistribuidorController::class, 'update']);
+Route::delete('/distribuidor/{id}', [DistribuidorController::class, 'delete']);
+Route::get('/distribuidor-find/{id}', [DistribuidorController::class, 'find']);
+Route::put('/distribuidor-search', [DistribuidorController::class, 'search']);
 
-Route::post('/producto', [ProductoController::class, 'create'])->middleware(ApiAuthMiddleware::class);
-Route::get('/productos', [ProductoController::class, 'list'])->middleware(ApiAuthMiddleware::class);
-Route::put('/producto/{id}', [ProductoController::class, 'update'])->middleware(ApiAuthMiddleware::class);
-Route::delete('/producto/{id}', [ProductoController::class, 'delete'])->middleware(ApiAuthMiddleware::class);
-Route::get('/producto-find/{id}', [ProductoController::class, 'find'])->middleware(ApiAuthMiddleware::class);
-Route::put('/producto-search', [ProductoController::class, 'search'])->middleware(ApiAuthMiddleware::class);
-Route::post('/producto/upload', [ProductoController::class, 'upload'])->middleware(ApiAuthMiddleware::class);
-Route::get('/producto/{filename}', [ProductoController::class, 'getImage'])->middleware(ApiAuthMiddleware::class);
+Route::post('/producto', [ProductoController::class, 'create']);
+Route::get('/productos', [ProductoController::class, 'list']);
+Route::put('/producto/{id}', [ProductoController::class, 'update']);
+Route::delete('/producto/{id}', [ProductoController::class, 'delete']);
+Route::get('/producto-find/{id}', [ProductoController::class, 'find']);
+Route::put('/producto-search', [ProductoController::class, 'search']);
+Route::post('/producto/upload', [ProductoController::class, 'upload']);
+Route::get('/producto/{filename}', [ProductoController::class, 'getImage']);
